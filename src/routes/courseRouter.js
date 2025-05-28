b7ebed68762a4cf09b5bbd8b7ac999a36662d3c4
Pratik <pgumthanavar05@gmail.com>
2025-05-28 16:09:01 +0530

    feat: implement course retrieval endpoints with validations

--- src/routes/courseRouter.js
+++ src/routes/courseRouter.js
@@ -2,7 +2,8 @@ const express = require('express');
 const courseRouter = express.Router();
 const { userAuth } = require('../middlewares/auth');
 const { validateCourseData } = require('../validations/validation');
-
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 const Course = require('../models/courseModel');
 
 courseRouter.post('/course/create', userAuth, async (req, res) => {
@@ -27,6 +28,49 @@ courseRouter.post('/course/create', userAuth, async (req, res) => {
   }
 });
 
-courseRouter.get('/course/view', userAuth, async (req, res) => {});
+courseRouter.get('/course/view/all', userAuth, async (req, res) => {
+  try {
+    const creatorId = req.user._id;
+
+    const courses = await Course.find({
+      creatorId,
+    }).select('title description price image creatorId');
+    if (courses.length === 0) {
+      res.send('No courses found!');
+    }
+
+    res.send({
+      message: 'Courses found!',
+      data: courses,
+    });
+  } catch (err) {
+    res.status(500).send('ERROR : ' + err.message);
+  }
+});
+
+courseRouter.get('/course/view/:courseId', userAuth, async (req, res) => {
+  try {
+    const courseId = req.params.courseId;
+
+    if (!ObjectId.isValid(courseId)) {
+      throw new Error('Invalid course Id!');
+    }
+    const course = await Course.find({
+      _id: courseId,
+      creatorId: req.user._id,
+    }).select('title description price image creatorId');
+
+    if (course.length === 0) {
+      res.send('No course found!');
+    }
+
+    res.send({
+      message: 'Course found!',
+      data: course,
+    });
+  } catch (err) {
+    res.status(500).send('ERROR : ' + err.message);
+  }
+});
 
 module.exports = courseRouter;
